9157a32f44f297feb25a3ec2e6ac114ae50120b2
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2024-01-03 02:10:30 +0530

    Day 1

---
+++ DSA/src/problemSolving/StringCharacterCount.java
@@ -0,0 +1,8 @@
+package problemSolving;
+
+public class StringCharacterCount {
+    public static void main(String[] args) {
+        String str="Ayush";
+        System.out.println(str.length());
+    }
+}
