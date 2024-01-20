e730062ae454b93cc9519778552833ba985d1f0f
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2024-01-20 23:36:39 +0530

    Spring Data Jpa Continue.....

--- Development/SpringDataJpa/src/main/java/com/narainox/SpringDataJpa/models/Author.java
+++ Development/SpringDataJpa/src/main/java/com/narainox/SpringDataJpa/models/Author.java
@@ -1,20 +1,66 @@
 package com.narainox.SpringDataJpa.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
 
 
 @NoArgsConstructor
 @Data
 @Entity
+@Table(name = "authors")
 public class Author {
+
     @Id
+    @GeneratedValue()
+    /*
+    @GeneratedValue(
+            strategy = GenerationType.TABLE,
+            generator = "author_id_gen"
+    )
+    @TableGenerator(
+            name = "author_id_gen",
+            table = "id_generator",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "author_sequence"
+    )
+
+    @SequenceGenerator(
+            name = "author_sequence",
+            sequenceName = "author_sequence",
+            allocationSize = 1
+    )
+    */
     private Integer id;
+    @Column(
+            name = "first_name",
+            length = 35
+    )
     private String firstName;
     private String lastName;
+    @Column(
+            unique = true,
+            nullable = false
+    )
     private String email;
     private Integer age;
+    @Column
+            (
+                    updatable = false,
+                    nullable = false
+            )
+    private LocalDateTime createdDate;
+    @Column
+            (
+            insertable = false
+            )
+    private LocalDateTime lastModified;
 }
