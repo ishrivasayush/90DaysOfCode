0f755874a980ae43dbc2028ac9cc1c137df43622
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2024-02-11 20:26:24 +0530

    Jpa start

--- Development/firstJobApp/src/main/java/com/narainox/firstJobApp/job/impl/JobServiceImpl.java
+++ Development/firstJobApp/src/main/java/com/narainox/firstJobApp/job/impl/JobServiceImpl.java
@@ -1,59 +1,52 @@
 package com.narainox.firstJobApp.job.impl;
 
 import com.narainox.firstJobApp.job.Job;
+import com.narainox.firstJobApp.job.JobRepository;
 import com.narainox.firstJobApp.job.JobService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JobServiceImpl implements JobService {
-    private List<Job> jobs=new ArrayList<>();
-    private long jobId=1L;
 
+    private JobRepository jobRepository;
+
+    public JobServiceImpl(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
     @Override
     public List<Job> findAll() {
-        return jobs;
+        return jobRepository.findAll();
     }
 
     @Override
     public void createJob(Job job) {
-        job.setId(jobId++);
-        jobs.add(job);
+        jobRepository.save(job);
     }
 
     @Override
     public Job findJobBYId(Long id) {
-        for (Job job:jobs)
-        {
-            if (job.getId()==id)
-            {
-                return job;
-            }
-        }
-        return null;
+       return jobRepository.findById(id).orElse(null);
     }
 
     @Override
     public boolean deleteJobByJobId(Long id) {
-        for (Job job:jobs)
-        {
-            if (job.getId()==id)
-            {
-                jobs.remove(job);
-                return true;
-            }
+        try {
+            jobRepository.deleteById(id);
+            return true;
         }
-        return false;
+        catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
     public boolean updateJob(Long id, Job job) {
-        for (Job jobb:jobs)
-        {
-            if (jobb.getId()==id)
-            {
+        Optional<Job> jobb=jobRepository.findById(id);
+        if (jobb.isPresent()){
                 jobb.setMaxSalary(job.getMaxSalary());
                 jobb.setDescription(job.getDescription());
                 jobb.setLocation(job.getLocation());
@@ -61,7 +54,9 @@ public class JobServiceImpl implements JobService {
                 jobb.setMinSalary(job.getMinSalary());
                 return true;
             }
+        else {
+            return false;
         }
-        return false;
-    }
+
 }
+
