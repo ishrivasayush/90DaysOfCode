1393bf094cd2d4dd64038e1f2e3aa59c7185d003
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2024-02-17 02:40:43 +0530

    Review Done

--- Development/firstJobApp/src/main/java/com/narainox/firstJobApp/review/ReviewService.java
+++ Development/firstJobApp/src/main/java/com/narainox/firstJobApp/review/ReviewService.java
@@ -5,7 +5,5 @@ import java.util.List;
 public interface ReviewService {
     List<Review> getAllReviews(Long companyId);
     String createReview(Long companyId,Review review);
-    Review  getReviewByReviewId(Long companyId,Long reviewId);
-    String deleteReview(Long companyId,Long reviewId);
     void updateReview(Long companyId, Review review, Long reviewId);
 }
